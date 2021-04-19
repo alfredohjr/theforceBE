7901b7daa387cf80e070c3173aceabe6e4b7a80f
alfredohjr <alfredojrgasper@gmail.com>
2021-04-18 21:44:44 -0300

    trabalhando com rotas

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import 'reflect-metadata';
+
+import './database';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (request, response) => {
     try {
