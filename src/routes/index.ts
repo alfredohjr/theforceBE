1a55d8c998be11855c3cae0244e4b787414eb4b5
alfredohjr <alfredojrgasper@gmail.com>
2021-05-02 19:34:50 -0300

    update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,9 @@ import usersRouter from './user.router';
 import loginRouter from './login.router';
 import logoutRouter from './logout.router';
 import productRouter from './product.router';
+import depositRouter from './deposit.router';
+import entityRouter from './entity.router';
+import documentRouter from './document.router';
 
 const routes = Router();
 
@@ -11,5 +14,8 @@ routes.use('/user', usersRouter);
 routes.use('/login', loginRouter);
 routes.use('/logout', logoutRouter);
 routes.use('/product', productRouter);
+routes.use('/deposit', depositRouter);
+routes.use('/entity', entityRouter);
+routes.use('/document', documentRouter);
 
 export default routes;
