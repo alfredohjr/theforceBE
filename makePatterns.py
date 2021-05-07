6d814b3f015ea8c1e042bb97e2f4912689a57532
alfredohjr <alfredojrgasper@gmail.com>
2021-05-07 19:51:28 -0300

    update patterns to create better Services files

--- makePatterns.py
+++ makePatterns.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 from patterns.model import Model
-from patterns.service import Services
+from patterns.service import Service
 from patterns.test import Test
 from patterns.router import Router
 from patterns.check import Check
@@ -25,33 +25,9 @@ if __name__ == '__main__':
         m = m.split('.')[0]
         model.append(m)
 
-    print(model)
-
-    print('Create Services Create, Update, Delete and IsValid')
-
-    types = ['Create','Update','Delete','IsValid','Get']
+    cs = Service()
 
     for m in model:
-
-        print('Create Services',m)
-        for t in types:
-            fCreate = open('{PATH}/services/{TYPE}{MODEL}Service.ts'.format(MODEL=m,TYPE=t,PATH=path),'w')
-            fCreate.write(Services().format(MODEL=m,MODELLOWER=m.lower(),TYPE=t))
-            fCreate.close()
-        
-        print('Create Model',m)
-        fCreate = open('{PATH}/models/{MODEL}.ts'.format(PATH=path,MODEL=m),'w')
-        fCreate.write(Model().format(MODEL=m,MODELLOWER=m.lower()))
-        fCreate.close()
-
-        print('Create __tests__',m)
-        fCreate = open('{PATH}/__tests__/{MODELLOWER}.router.spec.ts'.format(PATH=path,MODELLOWER=m.lower()),'w')
-        fCreate.write(Test())
-        fCreate.close()
-
-        print('Create routes',m)
-        fCreate = open('{PATH}/routes/{MODELLOWER}.router.ts'.format(PATH=path,MODELLOWER=m.lower()),'w')
-        fCreate.write(Router().format(MODELLOWER=m.lower()))
-        fCreate.close()
+        cs.execute(model=m)
     
     Check()
--- patterns/service.py
+++ patterns/service.py
@@ -1,18 +1,36 @@
 
-def Services():
+class Service:
 
-    text = '''// TODO:empty\n'''
-    text += '''import {{ getRepository }} from 'typeorm';\n'''
-    text += '''import {MODEL} from '{MODEL}.ts';\n\n'''
-    text += '''interface Request{{\n'''
-    text += '''    id: string;\n'''
-    text += '''}};\n\n'''
-    text += '''class {TYPE}{MODEL}Service {{\n'''
-    text += '''    public async execute({{id}}:Request): Promise<void>{{\n'''
-    text += '''        const {MODELLOWER}Repository = getRepository({MODEL});\n'''
-    text += '''\n'''
-    text += '''    }}\n'''
-    text += '''}}\n'''
-    text += '''export default {TYPE}{MODEL}Service;\n'''
+    def __init__(self):
+        self.create = self.getModel('create')
+        self.delete = self.getModel('delete')
+        self.isvalid = self.getModel('isvalid')
+        self.update = self.getModel('update')
+        self.tmpPath = 'tmp'
 
-    return text
+    def readModel(self,fullFilePath=None):
+        f = open(fullFilePath,'r')
+        f1 = f.readlines()
+        f.close()
+        return ''.join(f1)
+
+    def getModel(self,typeModel):
+        return self.readModel('patterns/service.model/' + typeModel + '.model')
+    
+    def write2file(self,fileName,text):
+        f = open(self.tmpPath + '/' + fileName,'w')
+        f.write(text)
+        f.close()
+
+    def execute(self,model=None):
+        text = self.create.format(MODEL=model,MODELLOWER=model.lower(),TYPE='Create')
+        self.write2file('services/Create' + model + 'Service.ts',text)
+
+        text = self.delete.format(MODEL=model,MODELLOWER=model.lower(),TYPE='Delete')
+        self.write2file('services/Delete' + model + 'Service.ts',text)
+
+        text = self.isvalid.format(MODEL=model,MODELLOWER=model.lower(),TYPE='IsValid')
+        self.write2file('services/IsValid' + model + 'Service.ts',text)
+
+        text = self.update.format(MODEL=model,MODELLOWER=model.lower(),TYPE='Update')
+        self.write2file('services/Update' + model + 'Service.ts',text)
