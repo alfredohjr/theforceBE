ef5a56fdad0bf6b233f4b50077abe777895bc6d2
alfredohjr <alfredojrgasper@gmail.com>
2021-05-17 23:55:10 -0300

    working with dotenv file and init email sending process

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import app from './app';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(`APP NAME: ${process.env.APP_NAME}`)
+
 app.listen(3333, () => {
     console.log('NumeroUm');
 });
