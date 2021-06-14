a41ae9b8dc9aae4161807fc95735786bf53bf258
alfredohjr <alfredojrgasper@gmail.com>
2021-06-13 21:08:16 -0300

    check if exists open document and stock in product before delete it.

--- src/Service/DeleteProductService.ts
+++ src/Service/DeleteProductService.ts
@@ -1,6 +1,10 @@
-import { getRepository } from "typeorm";
+import { Equal, getConnection, getRepository, Not } from "typeorm";
+import Document from "../models/Document";
+import DocumentProduct from "../models/DocumentProduct";
 import Product from "../models/Product";
 import ProductLog from "../models/ProductLog";
+import ProductPrice from "../models/ProductPrice";
+import Stock from "../models/Stock";
 
 interface Request {
     id: string;
@@ -10,11 +14,42 @@ interface Request {
 class DeleteProductService {
     public async execute({id, user_id}: Request): Promise<void> {
         const productRepository = getRepository(Product);
+        const stockRepository = getRepository(Stock);
+        const productPriceRepository = getRepository(ProductPrice);
 
         const productExists = await productRepository.findOne(id);
 
         if(!productExists) {
             throw new Error('product not found');
+        }
+
+        const stockIsDifferentOf0 = await stockRepository.findOne({
+            where: {
+                product_id: id,
+                amount: Not(Equal(0))
+            }
+        })
+
+        if(stockIsDifferentOf0) {
+            throw new Error('find product stock, delete abort');
+        }
+
+        const isOpenDocument1 = getRepository(Document)
+            .createQueryBuilder('documents')
+            .select('documents.id')
+            .where('documents.closed_at is null')
+            .andWhere('documents.deleted_at is null')
+        
+        const isOpenDocument = await getConnection()
+            .createQueryBuilder()
+            .select('id')
+            .from(DocumentProduct,'docprod')
+            .where('docprod.document_id in (' + isOpenDocument1.getQuery() + ')')
+            .andWhere('docprod.product_id = :id',{id})
+            .getMany();
+
+        if(isOpenDocument.length !== 0) {
+            throw new Error('find open document for this product, delete abort');
         }
 
         await productRepository.update(productExists.id,{
