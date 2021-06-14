8349dafac2ddba5b489f1ef147799e7c4326815d
alfredohjr <alfredojrgasper@gmail.com>
2021-06-13 21:02:02 -0300

    check if as moviment stock and open documents into deposit

--- src/Service/DeleteDepositService.ts
+++ src/Service/DeleteDepositService.ts
@@ -1,5 +1,9 @@
-import { getRepository } from "typeorm";
+// TODO: add deposit log service
+
+import { Equal, getRepository, MoreThan, Not } from "typeorm";
 import Deposit from "../models/Deposit";
+import Document from "../models/Document";
+import Stock from "../models/Stock";
 
 interface Request {
     id: string;
@@ -10,6 +14,8 @@ class DeleteDepositService {
     public async execute({id, user_id}: Request): Promise<void> {
 
         const depositRepository = getRepository(Deposit);
+        const stockRepository = getRepository(Stock);
+        const documentRepository = getRepository(Document)
 
         const depositExists = await depositRepository.findOne({
             where: {
@@ -20,6 +26,29 @@ class DeleteDepositService {
 
         if(!depositExists) {
             throw new Error('deposit not found');
+        }
+
+        const stockIsDifferentOf0 = await stockRepository.findOne({
+            where: {
+                deposit_id: id,
+                amount: Not(Equal(0)),
+            }
+        });
+
+        if(stockIsDifferentOf0) {
+            throw new Error('find movement stock in deposit, delete abort');
+        }
+
+        const isOpenDocument = await documentRepository.findOne({
+            where: {
+                deposit_id: id,
+                closed_at: null,
+                deleted_at: null
+            }
+        });
+
+        if(isOpenDocument) {
+            throw new Error('find open document for this deposit, delete abort');
         }
 
         await depositRepository.update(depositExists.id,{
