d3eab211fddfe441f00b94cdc6ad1bc863e98bcc
alfredohjr <alfredojrgasper@gmail.com>
2021-06-28 20:42:03 -0300

    add error middleware

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,13 @@ import 'reflect-metadata';
 import createConnection from './database';
 import routes from './routes';
 import uploadConfig from './config/upload';
+import ErrorMiddleware from './middlewares/ErrorMiddleware';
 
 createConnection();
 const app = express();
+
 app.use(express.json());
 app.use('/media', express.static(uploadConfig.directory))
-app.use(routes);
 
 app.get('/', (request, response) => {
     try {
@@ -18,5 +19,9 @@ app.get('/', (request, response) => {
         return response.status(400).json({message:err.message});
     }
 })
+
+app.use(routes);
+
+app.use(ErrorMiddleware);
 
 export default app;
